e4f40122a0cbc048e0e99026aa1757c57bbe1146
Perez <gabrieljesusperez@hotmail.com>
2021-07-19 09:54:58 -0300

    ajustes e remoção de função para otimização

--- src/gabrieljperez/Iugu/Customer.php
+++ src/gabrieljperez/Iugu/Customer.php
@@ -18,43 +18,20 @@ class Customer extends BaseCustomer
 
     /**
      * Cria uma o Token que é uma representação do meio de pagamento do cliente
+     * passando $test = true simula a criação do Token é uma representação do meio de pagamento do cliente
      *
-     * @param array $params
+     * @param  array   $params
+     * @param  boolean $test
      * @return array
      */
-    public function createPaymentToken(array $params)
+    public function createPaymentToken(array $params, $test = false)
     {
+        if ($test) {
+            $params['test'] = true;
+        }
+
         $this->setParams($params)->sendApiRequest('POST', 'payment_token');
 
         return $this->fetchResponse();
     }
-
-    /**
-     * Simula a criação do Token é uma representação do meio de pagamento do cliente
-     *
-     * @param  array $params
-     * @return array
-     */
-    public function createPaymentTokenSimulation(array $param) 
-    {
-        $params['test'] = true;
-        return $this->createPaymentToken($params);
-    }
-
-    /**
-     * Cria ou simula a criação de um token de pagamento filtrando
-     * pelo debug do projeto (dev || prod)
-     *
-     * @param  array $params
-     * @return array
-     */
-    public function createPaymentTokenDebug(array $params) 
-    {
-        if (config('app.debug')) {
-            return createPaymentTokenSimulation($params);
-        } else {
-            return createPaymentToken($params);
-        }
-    }
-
 }
